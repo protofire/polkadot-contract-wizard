4d5fc9e28e22a8db371c2152bc71fe792040989f
Agustín Longoni <agustin.longoni@altoros.com>
2023-01-31 16:09:09 -0300

    add basic stepper component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { Typography } from '@mui/material';
 import CustomizedButtons from 'src/view/components/Button';
+import Stepper from 'src/view/components/Stepper';
 
 export default function Home() {
   return (
@@ -7,7 +8,9 @@ export default function Home() {
       <Typography variant="h1" align="center" mt="2">
         Start building something amazing on Polkadot
       </Typography>
+      <Stepper />
       <CustomizedButtons></CustomizedButtons>
+      
     </>
   );
 }
---
+++ src/view/components/Stepper/index.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+
+const steps = [
+  'Plugins',
+  'Security',
+  'Deploy',
+];
+
+export default function MainStepper() {
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Stepper activeStep={1} alternativeLabel>
+        {steps.map((label) => (
+          <Step key={label}>
+            <StepLabel>{label}</StepLabel>
+          </Step>
+        ))}
+      </Stepper>
+    </Box>
+  );
+}
