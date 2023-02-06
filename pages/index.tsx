934e54aa9dd610c0554a5c667073e13613f73f3a
Henry Palacios <henrydpalacios@gmail.com>
2023-02-06 10:31:11 -0300

    Extract home button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
-import { Typography } from '@mui/material';
+import { Stack, Typography } from '@mui/material';
 import CustomizedButtons from 'src/view/components/Button';
+import { HomeButton } from '@components';
+import { TOKEN_PATHS } from '@constants';
 
 export default function Home() {
   return (
@@ -7,6 +9,15 @@ export default function Home() {
       <Typography variant="h1" align="center" mt="2">
         Start building something amazing on Polkadot
       </Typography>
+      <Stack spacing={4} direction="column" m={8}>
+        <HomeButton title='TOKEN | PSP22' subtitle='Standard smart contract for a fungible token'
+          imgPath={TOKEN_PATHS.TokenIcon} imgProps={{ width: 75, height: 65 }} />
+        <HomeButton title='NFT | PSP34' subtitle='Standard smart contract for a non-fungible token'
+          imgPath={TOKEN_PATHS.NFTIcon} imgProps={{ width: 75, height: 65 }} />
+        <HomeButton title='MULTITOKEN | PSP37' subtitle='Standard smart contract for a Multi Token'
+          imgPath={TOKEN_PATHS.MultiTokenIcon} imgProps={{ width: 75, height: 65 }} />
+      </Stack>
+
       <CustomizedButtons></CustomizedButtons>
     </>
   );
---
+++ src/view/components/HomeButton/index.tsx
@@ -0,0 +1,63 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Button, { ButtonProps } from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import ArrowBackRoundedIcon from '@mui/icons-material/ArrowBackRounded';
+import TokenIcon from 'public/assets/token-psp22.png';
+import NFTIcon from 'public/assets/nft-psp34.png';
+import MultiTokenIcon from 'public/assets/multitoken-psp37.png';
+
+import Image from 'next/image';
+import { Typography } from '@mui/material';
+
+interface Props {
+    title: string
+    subtitle: string
+    imgPath: string
+    imgProps: { width?: number, height?: number }
+}
+
+const WrapperButton = styled(Button)<ButtonProps>(({ theme }) => ({
+    color: 'white',
+    fontSize: '1.4rem',
+    borderRadius: '1rem',
+    minWidth: '100%',
+    backgroundColor: 'transparent',
+    display: 'flex',
+    alignItems: 'center',
+    margin: 'auto',
+    position: 'relative',
+    padding: '2rem',
+    border: 'solid 1px transparent',
+    backgroundImage:
+        'linear-gradient(rgba(255, 255, 255, 0), rgba(255, 255, 255, 0)), linear-gradient(180deg, #B214AC, #8C7524)',
+    backgroundOrigin: 'border-box',
+    backgroundClip: 'content-box, border-box',
+    boxShadow: '2px 1000px 1px #0D0E13 inset',
+
+    '&:hover': {
+        backgroundImage:
+            'linear-gradient(rgba(255, 255, 255, 0), rgba(255, 255, 255, 0)), linear-gradient(90deg, #ffffff, #ffb7ff)',
+        backgroundOrigin: 'border-box',
+        backgroundClip: 'content-box, border-box',
+        boxShadow: '2px 1000px 1px #11121a inset',
+    },
+}));
+
+export const HomeButton = (props: Props) => {
+    const { title, subtitle, imgProps, imgPath } = props
+
+    return (
+        <WrapperButton variant="contained">
+            <Stack spacing={2} direction="row" alignItems="center">
+                <Image alt={title} src={imgPath} {...imgProps} />
+                <Stack spacing={0} direction="column" alignItems="flex-start">
+                    <Typography variant="h3">{title}</Typography>
+                    <Typography variant="subtitle1">
+                        {subtitle}
+                    </Typography>
+                </Stack>
+            </Stack>
+        </WrapperButton>
+    )
+}
