9085e0d3eba62c3253822b3b12634838cc8ca39c
Agustín Longoni <agustin.longoni@protofire.io>
2023-04-03 16:49:33 -0300

    [Sidebar] Protofire logo +  Telegram support (#47)
    * add protofire logo
    
    * add profire logo and need help?
    
    * remove import

--- src/view/layout/MainDrawer/DrawerContent.tsx
+++ src/view/layout/MainDrawer/DrawerContent.tsx
@@ -1,24 +1,71 @@
 import { useRouter } from 'next/router'
-
+import Image from 'next/image'
 import Navigation from './Navigation'
 import SimpleBar from 'src/view/components/third-party/SimpleBar'
+import { LOGO_PROTOFIRE } from 'src/constants/images'
+import { Typography, Stack, Link } from '@mui/material'
+import TelegramIcon from '@mui/icons-material/Telegram'
 
 const DrawerContent = () => {
   const { pathname } = useRouter()
 
   return (
-    <SimpleBar
-      sx={{
-        '& .simplebar-content': {
+    <>
+      <SimpleBar
+        sx={{
+          '& .simplebar-content': {
+            display: 'flex',
+            flexDirection: 'column',
+            width: '100%',
+            marginTop: '2rem'
+          }
+        }}
+      >
+        <Navigation currentPath={pathname} />
+      </SimpleBar>
+      <Stack
+        sx={{
           display: 'flex',
           flexDirection: 'column',
-          width: '100%',
-          marginTop: '2rem'
-        }
-      }}
-    >
-      <Navigation currentPath={pathname} />
-    </SimpleBar>
+          padding: '1.2rem'
+        }}
+      >
+        <Typography variant="h5" mb={4}>
+          <Link
+            href="https://t.me/+u5M4K7vKfbQxZjMx"
+            underline="hover"
+            target="_blank"
+            rel="noopener noreferrer"
+            sx={{
+              color: '#ffffff',
+              display: 'flex',
+              alignItems: 'center',
+              gap: '1rem'
+            }}
+          >
+            <TelegramIcon fontSize="small" sx={{ color: '#ffffff' }} />
+            Need Help?
+          </Link>
+        </Typography>
+
+        <Typography variant="body1" sx={{ color: '#ffffff7d' }}>
+          Developed by
+        </Typography>
+        <Link
+          href="https://protofire.io"
+          underline="hover"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            alt="Logo protofire"
+            src={LOGO_PROTOFIRE}
+            width={100}
+            height={25}
+          />
+        </Link>
+      </Stack>
+    </>
   )
 }
 
