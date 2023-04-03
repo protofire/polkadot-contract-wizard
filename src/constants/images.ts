ff03be8fcbdd0e0aa08813d8add61662b3ca2a56
Agustín Longoni <agustin.longoni@altoros.com>
2023-04-03 16:31:23 -0300

    add profire logo and need help?

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,5 +1,6 @@
 export const LOGO_POLKADOT = '/assets/polka_logo_horizontal.svg'
 export const CW_POLKADOT = '/assets/CW_logo.svg'
+export const LOGO_PROTOFIRE = '/assets/protofire.svg'
 export const GIF_COMPILING = '/assets/compiling.gif'
 export const SVG_SUCCESSFULLY = '/assets/successfully.svg'
 export const SVG_AWESOME = '/assets/auto_awesome.svg'
