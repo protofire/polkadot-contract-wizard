85ab999c07e3ffbd5813dea0af6b0903053adc33
Henry Palacios <henrydpalacios@gmail.com>
2023-04-26 20:13:43 -0300

    Changing Content Security Policies

--- next.config.js
+++ next.config.js
@@ -1,17 +1,41 @@
 /** @type {import('next').NextConfig} */
+const { nanoid } = require('nanoid')
+const crypto = require('crypto')
+
+const hash = crypto.createHash('sha256')
+hash.update(nanoid())
+const nonce = hash.digest('base64')
+const production = process.env.NODE_ENV === 'production'
+const rpcUrl = production
+  ? process.env.NEXT_PUBLIC_PROVIDER_SOCKET_PROD
+  : process.env.NEXT_PUBLIC_PROVIDER_SOCKET_DEV
+
+const ContentSecurityPolicy = `
+  default-src 'self';
+  base-uri 'self';
+  style-src 'self' 'unsafe-inline';
+  script-src 'nonce-${nonce}' 'self' ${production ? '' : "'unsafe-eval'"};
+  child-src  ${process.env.NEXT_PUBLIC_DOMAIN};
+  font-src 'self';  
+  connect-src ${rpcUrl} ${process.env.NEXT_PUBLIC_BACKEND_API} ${!production ? "'self'" : null}
+`
+
 const nextConfig = {
   reactStrictMode: true,
-  async headers () { 
-    return [{
-      source: '/(.*)',
-      headers: [{
-        key: 'Content-Security-Policy',
-        value:
-          `default-src 'self'; script-src 'self' ${process.env.NEXT_PUBLIC_BACKEND_API}; `,
-        }
-      ]
-    }]
+  // Adding policies:
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
+          },
+        ]
+      },
+    ]
   }
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
