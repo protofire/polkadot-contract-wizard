de2b2529d5d629fbb181616bc201340e28ea49fd
Henry Palacios <henrydpalacios@gmail.com>
2023-05-02 15:12:25 -0300

    Adding backend URL environment

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+const backendApi = `${process.env.NEXT_PUBLIC_BACKEND_API}/:path*`
+
 const nextConfig = {
   reactStrictMode: true,
   async rewrites() {
     return [
-      { source: '/api/:path*', destination: '/:path*' },
+      { source: '/api/:path*', destination: backendApi },
     ]
   }
 };
