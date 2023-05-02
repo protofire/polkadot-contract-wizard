4ea66c83115aba1039ce1a991cc3a3b3aaaa848f
Henry Palacios <henrydpalacios@gmail.com>
2023-05-02 12:54:46 -0300

    Rewrites backend domain

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      { source: '/api/:path*', destination: '/:path*' },
+    ]
+  }
 };
 
 module.exports = nextConfig;
