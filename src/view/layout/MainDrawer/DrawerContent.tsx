4533f4517981235fd2dd27b9757969021d8d3514
Agustín Longoni <agustin.longoni@altoros.com>
2023-05-05 13:22:46 -0300

    fix developed by design

--- src/view/layout/MainDrawer/DrawerContent.tsx
+++ src/view/layout/MainDrawer/DrawerContent.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import Navigation from './Navigation'
 import SimpleBar from 'src/view/components/third-party/SimpleBar'
 import { LOGO_PROTOFIRE } from 'src/constants/images'
-import { Typography, Stack, Link } from '@mui/material'
+import { Typography, Stack, Link, Box } from '@mui/material'
 import TelegramIcon from '@mui/icons-material/Telegram'
 
 const DrawerContent = () => {
@@ -47,23 +47,34 @@ const DrawerContent = () => {
             Need Help?
           </Link>
         </Typography>
-
-        <Typography variant="body1" sx={{ color: '#ffffff7d' }}>
-          Developed by
-        </Typography>
-        <Link
-          href="https://protofire.io"
-          underline="hover"
-          target="_blank"
-          rel="noopener noreferrer"
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'center',
+            gap: '0.5rem'
+          }}
         >
-          <Image
-            alt="Logo protofire"
-            src={LOGO_PROTOFIRE}
-            width={100}
-            height={25}
-          />
-        </Link>
+          <Typography
+            variant="body1"
+            sx={{ color: '#ffffff7d', fontSize: '0.8rem', marginTop: '0.1rem' }}
+          >
+            Developed by
+          </Typography>
+          <Link
+            href="https://protofire.io"
+            underline="hover"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              alt="Logo protofire"
+              src={LOGO_PROTOFIRE}
+              width={100}
+              height={25}
+            />
+          </Link>
+        </Box>
       </Stack>
     </>
   )
