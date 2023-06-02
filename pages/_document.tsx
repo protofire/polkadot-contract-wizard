0ae84ede17e990bdb287e74ab52e412b9bbf3222
Henry Palacios <henrydpalacios@gmail.com>
2023-06-02 10:51:47 -0300

    Adding privacy-friendly analytics (#108)
    * Adding plausible analytics
    
    * Using next-plausible package

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="description" content={`Polkadot Contract Wizard`} />
+        <meta
+          name="keywords"
+          content="Polkadot, Smart Contracts, code builder"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
