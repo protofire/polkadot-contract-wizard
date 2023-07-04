28858d4cde7e924cdc1473d9429361841e5490b2
Henry Palacios <henrydpalacios@gmail.com>
2023-07-04 18:48:34 -0300

    Mocking useViewportWidth in test Render Page Docs

--- tests/sections/docs/index.test.tsx
+++ tests/sections/docs/index.test.tsx
@@ -1,6 +1,10 @@
 import { render, screen } from '@testing-library/react'
 import Docs from '@/pages/docs/index'
 
+jest.mock('@/hooks/useViewportWidth', () => ({
+  useViewportWidth: () => ({ viewportWidth: 1024 })
+}))
+
 describe('Docs sections', () => {
   it('renders a heading', () => {
     render(<Docs />)
