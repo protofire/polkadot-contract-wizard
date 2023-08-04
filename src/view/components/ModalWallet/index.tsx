338c2876c0b4307c060ed1d20b69de6019aa0032
bogos <andre.neyra@gmail.com>
2023-08-04 16:18:17 -0500

    move modal styles to his own file

--- src/view/components/ModalWallet/index.tsx
+++ src/view/components/ModalWallet/index.tsx
@@ -2,49 +2,19 @@ import React from 'react'
 import {
   Avatar,
   Box,
-  Divider,
-  List,
   ListItem,
-  ListItemButton,
   ListItemIcon,
   ListItemText,
-  Modal,
-  Typography
+  Modal
 } from '@mui/material'
 import { Wallet } from '@/infrastructure/useink/walletTypes'
-
-const modalStyle = {
-  position: 'absolute' as const,
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 680,
-  height: 600,
-  textAlign: 'justify',
-  bgcolor: 'rgba(0, 0, 0, 1)',
-  border: '2px solid #000',
-  borderRadius: '3.75rem',
-  padding: '3rem 3rem 2.5rem 3rem',
-  boxShadow: '0px 4px 50px 0px rgba(255, 255, 255, 0.1);',
-  color: 'white',
-  display: 'flex',
-  flexDirection: 'column'
-}
-
-const listItemSx = {
-  '&:hover': {
-    borderRadius: '1.8rem',
-    backgroundColor: 'rgba(98, 98, 98, 0.26)'
-  }
-}
-
-const listSx = {
-  margin: '0 auto',
-  width: '16rem',
-  '&:hover': {
-    borderRadius: '1.8rem'
-  }
-}
+import {
+  ModalStyled,
+  ModalStyledDivider,
+  ModalStyledList,
+  ModalStyledListItem,
+  ModalTypography
+} from './styled'
 
 type Props = {
   open: boolean
@@ -62,33 +32,17 @@ export function ModalWallet({
   const walletNotInstalled = wallets.filter(wallet => !wallet.installed)
   return (
     <Modal open={open} onClose={handleClose}>
-      <Box sx={modalStyle}>
-        <Typography
-          style={{ textAlign: 'center' }}
-          id="modal-modal-title"
-          variant="h3"
-          component="h2"
-        >
+      <ModalStyled>
+        <ModalTypography id="modal-modal-title" variant="h3">
           Connect your wallet
-        </Typography>
+        </ModalTypography>
         <Box>
-          <Typography
-            style={{
-              textAlign: 'center',
-              fontWeight: 'normal',
-              marginTop: '2rem',
-              marginBottom: '1.5rem'
-            }}
-            variant="h6"
-          >
-            Installed Wallets
-          </Typography>
-          <List disablePadding sx={listSx}>
+          <ModalTypography variant="h6">Installed Wallets</ModalTypography>
+          <ModalStyledList disablePadding>
             {walletInstalled.map(w => (
               <ListItem key={w.title}>
                 <>
-                  <ListItemButton
-                    sx={listItemSx}
+                  <ModalStyledListItem
                     onClick={() => {
                       setCurrentWallet(w)
                       handleClose()
@@ -98,20 +52,19 @@ export function ModalWallet({
                       <Avatar src={w.logo.src} alt={w.logo.alt} />
                     </ListItemIcon>
                     <ListItemText primary={`${w.title}`} />
-                  </ListItemButton>
+                  </ModalStyledListItem>
                 </>
               </ListItem>
             ))}
-          </List>
+          </ModalStyledList>
         </Box>
         <Box>
-          <Divider style={{ margin: '2rem' }} variant="middle" />
-          <List disablePadding sx={listSx}>
+          <ModalStyledDivider variant="middle" />
+          <ModalStyledList disablePadding>
             {walletNotInstalled.map(w => (
               <ListItem key={w.title}>
                 <>
-                  <ListItemButton
-                    sx={listItemSx}
+                  <ModalStyledListItem
                     onClick={() => {
                       setCurrentWallet(w)
                       handleClose()
@@ -121,13 +74,13 @@ export function ModalWallet({
                       <Avatar src={w.logo.src} alt={w.logo.alt} />
                     </ListItemIcon>
                     <ListItemText primary={`Install ${w.title}`} />
-                  </ListItemButton>
+                  </ModalStyledListItem>
                 </>
               </ListItem>
             ))}
-          </List>
+          </ModalStyledList>
         </Box>
-      </Box>
+      </ModalStyled>
     </Modal>
   )
 }
