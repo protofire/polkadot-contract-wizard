f9d4a38cf43b0e39db6d492a0ca79cff225b2baf
Agustín Longoni <agustin.longoni@altoros.com>
2023-10-23 16:31:43 -0300

    fix breackpoint sizes

--- src/view/layout/MainContainer.tsx
+++ src/view/layout/MainContainer.tsx
@@ -4,7 +4,7 @@ const MainContainer = ({ children }: { children: React.ReactNode }) => {
   return (
     <Box
       sx={{
-        width: { sm: '90%', md: '75%', lg: '100%', xl: '75%' },
+        width: { sm: '90%', md: '95%', lg: '100%', xl: '75%' },
         margin: { lg: '1rem auto 2rem auto', xl: '2rem auto 2rem auto' }
       }}
     >
