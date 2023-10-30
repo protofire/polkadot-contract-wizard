64a263ecb7864a4d5a5c1303171de3842d5e6020
Agustín Longoni <agustin.longoni@altoros.com>
2023-10-30 14:24:56 -0300

    skeleton improvements

--- src/view/CustomContracts/detail/SkeletonContractDetail.tsx
+++ src/view/CustomContracts/detail/SkeletonContractDetail.tsx
@@ -11,29 +11,27 @@ export function ContractDetailSkeleton() {
       }}
     >
       <Stack direction="row" justifyContent="space-between">
-        <MySkeleton variant="text" width={200} height={40} />
-        <Stack direction="row" spacing={2}>
-          <MySkeleton variant="rectangular" width={40} height={40} />
-          <MySkeleton variant="rectangular" width={40} height={40} />
-          <MySkeleton variant="rectangular" width={40} height={40} />
-        </Stack>
-        <MySkeleton variant="text" width={150} height={20} />
+        <MySkeleton variant="text" width={200} height={50} />
+        <MySkeleton variant="text" width={150} height={30} />
       </Stack>
       <Stack direction="row" spacing={2}>
-        <MySkeleton variant="text" width={300} height={20} />
-        <MySkeleton variant="rectangular" width={40} height={40} />
+        <MySkeleton variant="text" width={300} height={30} />
       </Stack>
-      <Box mt={2}>
-        <MySkeleton variant="rectangular" height={20} />
-        <MySkeleton variant="rectangular" height={20} />
-        <MySkeleton variant="rectangular" height={20} />
-        <MySkeleton variant="rectangular" height={20} />
+
+      <Box mt={4}>
+        <Stack direction="row" width={'100%'} spacing={2}>
+          <MySkeleton variant="rectangular" width={'25%'} height={60} />
+          <MySkeleton variant="rectangular" width={'25%'} height={60} />
+          <MySkeleton variant="rectangular" width={'25%'} height={60} />
+          <MySkeleton variant="rectangular" width={'25%'} height={60} />
+        </Stack>
       </Box>
-      <Box mt={2}>
-        <MySkeleton variant="text" width={200} height={40} />
-        <MySkeleton variant="text" width={300} height={20} />
-        <MySkeleton variant="rectangular" height={100} />
-        <MySkeleton variant="rectangular" height={100} />
+      <Box mt={8}>
+        <Stack direction="row" width={'100%'} spacing={1}>
+          <MySkeleton variant="rectangular" width={'25%'} height={60} />
+          <MySkeleton variant="rectangular" width={'25%'} height={60} />
+        </Stack>
+        <MySkeleton variant="rectangular" height={350} />
       </Box>
     </Box>
   )
