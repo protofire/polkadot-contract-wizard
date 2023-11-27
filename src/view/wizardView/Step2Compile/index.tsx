b17a418e23a05cfd75d9a76fc6f2db5f97b86b3a
Henry Palacios <henrydpalacios@gmail.com>
2023-11-27 17:12:15 -0300

    Testing fix cache deploy problems

--- src/view/wizardView/Step2Compile/index.tsx
+++ src/view/wizardView/Step2Compile/index.tsx
@@ -1,6 +1,6 @@
 import { useMemo } from 'react'
-import { Box, Typography } from '@mui/material'
 import { styled } from '@mui/material/styles'
+import { Box, Typography } from '@mui/material'
 import { CopyBlock, atomOneDark } from 'react-code-blocks'
 
 import { useStepsSCWizard } from '@/context'
